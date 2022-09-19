f476fbfdd798fdeed5bbc1193e654a4d9b2f80a3
LukaPavicic1 <91119924+LukaPavicic1@users.noreply.github.com>
2022-09-19 07:28:26 +0200

    Add files via upload

---
+++ ponavljanje6/ponavljanje6/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ponavljanje6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int i = 1;
+            Console.WriteLine("Unesi broj članova reda za sumu: ");
+            int brClanova = Convert.ToInt32(Console.ReadLine());
+
+            do
+            {
+                Console.Write(" + 1/" + Math.Pow(3, i));
+                i++;
+
+            }
+            while (i <= brClanova);
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
